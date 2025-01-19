7528b54855e4e2dc40e28ddeb07ee249b648d3c6
Raeyaan A. <syedraeyaan@outlook.com>
2025-01-19 21:16:41 +0530

    Add feature to choose the number of games to save & alter the introductory message

--- python/game_name_storing.py
+++ python/game_name_storing.py
@@ -1,9 +1,11 @@
-print("Enter 5 games which you like")
+print("Enter games which you like")
 
 game = [] # Empty list to store 5 game name
 name = "" # Empty string to store individual game name
 
-for n in range (0,5):
+choice = int(input("How many games would you like to save ? "))
+
+for n in range (0,choice):
     name = input("Name: ")
     game.append(name)
 
