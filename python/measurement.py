6079de050693e68291a6244bf99167b6eb314c85
syedraeyaan <syedraeyaan@outlook.com>
2025-01-21 13:26:23 +0000

    Add triangle functions

--- python/measurement.py
+++ python/measurement.py
@@ -22,3 +22,25 @@ def area_straightQuad(l:float, w:float):
 def perimeter_straightQuad(l:float, w:float):
     return 2*(l+w)
 
+# Triangle functions
+
+def triangle_area(s1:float, s2:float, s3:float):
+   if s1 + s2 > s3 and s2 + s3 > s1: 
+    
+    semi = (s1+s2+s3)/3
+    return math.sqrt(semi*(semi-s1)*(semi-s2)*(semi-s3))
+   
+   else:
+    return "Invalid Triangle"
+
+def triangle_perimeter(s1:float, s2:float, s3:float):
+     if s1 + s2 > s3 and s2 + s3 > s1: 
+       return s1 + s2 +s3
+     else:
+       return "Invalid Triangle"
+
+def height_triangle (s1:float, s2:float, base:float):
+     if s1 + s2 > base and s2 + base > s1: 
+       return triangle_area(s1,s2,base)/(0.5*base)
+     else:
+       return "Invalid Triangle"
