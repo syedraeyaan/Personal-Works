054a90ebc9d9b4fc33c513b5a8ba693c37c507ef
syedraeyaan <syedraeyaan@outlook.com>
2025-01-22 11:53:18 +0000

    Initial commit

---
+++ python/caesar_cipher.py
@@ -0,0 +1,22 @@
+import string
+
+letters = list(string.ascii_lowercase)
+
+def caesar_cipher_encrypt(original_text:str , offset:int): # Defines the parameters
+    
+    caesar_text = "" # Empty String to store the new alphabet
+    
+    for i in original_text:
+        
+        letter_index = letters.index(i)
+        caesar_index = letter_index + offset
+        
+        caesar_text += letters[caesar_index] # Concatenation of the alphabets to build the caesar_text
+    
+    return caesar_text # Returns the encrypted string as the output
+
+message = input("Enter the message to encrypt: \n").lower()
+offset_input = int(input("Enter the offset number: \n"))
+
+print(f"Your original text: {message}")
+print(f"Encrypted Message: {caesar_cipher_encrypt(message, offset_input)}")
