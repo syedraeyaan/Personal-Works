312ddb50f2d0be77f5bbfc63e2a2c6ed10ff37f5
syedraeyaan <syedraeyaan@outlook.com>
2025-01-22 12:08:04 +0000

    Debug & add new feature

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -2,21 +2,31 @@ import string
 
 letters = list(string.ascii_lowercase)
 
-def caesar_cipher_encrypt(original_text:str , offset:int): # Defines the parameters
-    
-    caesar_text = "" # Empty String to store the new alphabet
-    
-    for i in original_text:
-        
-        letter_index = letters.index(i)
-        caesar_index = letter_index + offset
-        
-        caesar_text += letters[caesar_index] # Concatenation of the alphabets to build the caesar_text
-    
-    return caesar_text # Returns the encrypted string as the output
+work = input("Type encode to encrypt the message / decode to decrypt the message: ").lower()
 
-message = input("Enter the message to encrypt: \n").lower()
-offset_input = int(input("Enter the offset number: \n"))
+if work == "encode":
 
-print(f"Your original text: {message}")
-print(f"Encrypted Message: {caesar_cipher_encrypt(message, offset_input)}")
+    def caesar_cipher_encrypt(original_text:str , offset:int): # Defines the parameters
+        
+        caesar_text = "" # Empty String to store the new alphabet
+        
+        for i in original_text:
+            
+            letter_index = letters.index(i)
+            caesar_index = letter_index + offset
+            
+            caesar_text += letters[caesar_index] # Concatenation of the alphabets to build the caesar_text
+        
+        return caesar_text # Returns the encrypted string as the output
+
+    message = input("Enter the message to encrypt: \n").lower()
+    offset_input = int(input("Enter the offset number: \n"))
+
+    print(f"Your original text: {message}")
+    print(f"Encrypted Message: {caesar_cipher_encrypt(message, offset_input)}")
+
+elif work == "decode":
+    print("We are currently working on this feature !")
+
+else:
+    print("Invalid Input")
