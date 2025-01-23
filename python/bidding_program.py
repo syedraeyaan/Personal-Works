f35dcf59bf76f77c1143724338bae4a8666a5fc3
syedraeyaan <syedraeyaan@outlook.com>
2025-01-23 07:58:01 +0000

    Add feature to extend the number of keys and their respective values

--- python/bidding_program.py
+++ python/bidding_program.py
@@ -4,3 +4,22 @@
 # Check for the highest bid
 # Print the name of bidder & their respective bid price
 
+bid = {} # Empty bid dictionary
+
+continue_bidding = True  # Assigned True boolean value to continue_bidding variable
+
+while continue_bidding == True: # While loop runs when the statement passed is true and in this case continue_bidding is set to true.
+   
+   bidder = input("Enter your name: ") # Input as string
+   bid_price = float(input("Enter the bid price: Rs")) # Price of bid in Rs
+   
+   bid[bidder] = bid_price # Assigns value to key
+
+   bidding_process = input("Y - Continue Bid Process | N - Stop bid process: ")
+
+   if bidding_process == 'Y':
+      continue_bidding = True
+   elif bidding_process == 'N':
+      continue_bidding = False
+
+print(bid)
