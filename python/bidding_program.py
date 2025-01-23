b85d96ea78f6047d89c099475570b98de2b9d9aa
syedraeyaan <syedraeyaan@outlook.com>
2025-01-23 08:36:51 +0000

    Add feature to find out the highest bid and bidder

--- python/bidding_program.py
+++ python/bidding_program.py
@@ -13,13 +13,30 @@ while continue_bidding == True: # While loop runs when the statement passed is t
    bidder = input("Enter your name: ") # Input as string
    bid_price = float(input("Enter the bid price: Rs")) # Price of bid in Rs
    
-   bid[bidder] = bid_price # Assigns value to key
+   bid[bidder] = bid_price # Assigns value (bid_price) to key (bidder)
 
-   bidding_process = input("Y - Continue Bid Process | N - Stop bid process: ")
+   bidding_process = input("Y - Continue Bid Process | N - Stop bid process: ").lower()
 
-   if bidding_process == 'Y':
+   if bidding_process == 'y':
       continue_bidding = True
-   elif bidding_process == 'N':
+   elif bidding_process == 'n':
+      continue_bidding = False
+   else:
+      print("Invalid Statement")
       continue_bidding = False
 
-print(bid)
+print("Do you want to know the highest bidder ?")
+
+highest_bidder = input("y - to know the highest bidder | n - cancel: ").lower()
+
+super_bidder = max(bid, key = bid.get)
+
+if highest_bidder == 'y':
+   print(f"Highest bidder is {super_bidder} with bid price = Rs {bid[super_bidder]}")
+   print(bid)
+
+elif highest_bidder == 'n':
+   print(bid)
+
+else:
+   print("Invalid Input")
