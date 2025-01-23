64ac8b920a749b4f6d2d046091b0a5eb0aaf4899
syedraeyaan <syedraeyaan@outlook.com>
2025-01-23 06:57:45 +0000

    Initial Commit | Add comments for better readability

---
+++ python/bidding_program.py
@@ -0,0 +1,6 @@
+# Bidding program
+# Take input as name & bid price
+# Continue taking input until the user says no
+# Check for the highest bid
+# Print the name of bidder & their respective bid price
+
